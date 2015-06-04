4c783cc155515ffd127c763ec84528d19b28cdf5
DrunkenIbris <PRMBN@MD12D67C.ww002.siemens.net>
2015-06-04 08:11:53 +0200

    Test version

---
+++ Halloworld35/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Halloworld35
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hallo world!");
+
+            Console.ReadLine();
+        }
+    }
+}
